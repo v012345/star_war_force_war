a527380f55396e6f429092a470a9f59503e51ebe
v138468 <v138468@gmail.com>
2022-02-21 17:36:46 +0800

    i

---
+++ assets/scripts/explore.ts
@@ -0,0 +1,37 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    // @property(cc.Label)
+    // label: cc.Label = null;
+
+    // @property
+    // text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start() {
+
+    }
+    back() {
+        if (window.scence) {
+            window.scence.pop();
+            cc.director.loadScene(window.scence[window.scence.length - 1]);
+        } else {
+            cc.director.loadScene("login");
+        }
+    }
+
+
+    // update (dt) {}
+}
--- assets/scripts/login.ts
+++ assets/scripts/login.ts
@@ -5,23 +5,50 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
+import axios from "axios";
+declare global {
+    interface Window { scence: Array<string>; }
+}
 
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    // @property(cc.Label)
+    // label: cc.Label = null;
 
-    @property
-    text: string = 'hello';
+    // @property(cc.Node)
+    // button: cc.Node = null;
+
+    @property(cc.Label)
+    account: cc.Label = null;
+
+    private isLogingin: boolean = false;
+
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
 
-    start () {
+    login(): void {
 
+        if (this.account.string && !this.isLogingin) {
+            this.isLogingin = true
+            axios.post("http://api.nightowl.name/api/user/login").then(r => {
+                // console.log(r.data)
+                window.scence.push("hall");
+                cc.director.loadScene("hall");
+            })
+        }
+
+    }
+
+    start(): void {
+        window.scence = [];
+        // console.log(this);
+        // axios.post("http://api.nightowl.name/api/user/login").then(r => {
+        //     console.log(r.data)
+        // })
     }
 
     // update (dt) {}
